02043a5d02de7cc38794961adee4c903910b1904
SagallaBrian <brayozwebdesigner@gmail.com>
2022-09-17 17:42:44 +0300

    django project

--- testa/settings.py
+++ testa/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^1!-o2kjqjdxq-^9s)pazp#e28dx5b)=c98i0jv9@i!b_1z-y&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
